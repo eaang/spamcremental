196e35b3742b11eef0532ea0f4ad7f6e4b2be8a8
Evan Ang <alethiologist@gmail.com>
2020-09-08 22:36:57 +0800

    fixed rate

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,12 @@
 <template>
-  <div id="counter-container" class="grid grid-cols-2">
+  <div id="counter-container" class="grid grid-cols-3 space-x-4">
     <div id="mail-counter">
       <div class="font-bold text-center">Mails Sent</div>
       <div class="text-center">{{ spamSent }}</div>
+    </div>
+    <div id="rate-counter">
+      <div class="font-bold text-center">$$$ Per Second</div>
+      <div class="text-center">{{ rate }}</div>
     </div>
     <div id="money-counter">
       <div class="font-bold text-center">Cash Avaliable</div>
@@ -16,6 +20,14 @@ export default {
   computed: {
     spamSent() {
       return this.$store.state.mail;
+    },
+    rate() {
+      const rate = (this.$store.state.mail / 1000) * this.$store.state.moneyPerBatch
+      if (rate < 0.01) {
+        return '< $0.01';
+      } else {
+        return '$' + rate.toFixed(2);
+      }
     },
     moneyMade() {
       return this.$store.state.money;
